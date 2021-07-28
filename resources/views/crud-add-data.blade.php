a0f845bedeef5d7cad6efbab059ca1387b11cda5
aliansyah <muhammad.aliansyah@divusi.com>
2021-07-28 21:57:20 +0700

    Remove unused code

--- resources/views/crud-add-data.blade.php
+++ resources/views/crud-add-data.blade.php
@@ -36,17 +36,12 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    {{-- <label>Nama Barang</label>
-                                    <input type="text" name="inventory_name" class="form-control"> --}}
-                                    {{-- Utk mengubah warna text mjd merah jika ada error --}}
                                     <label @error('inventory_name') class="text-danger" @enderror>
                                         Nama Barang
-                                        {{-- Jika ada error, print pesan error --}}
                                         @error('inventory_name')
                                             | {{ $message }}
                                         @enderror
                                     </label>
-                                    {{-- old('attr-name') berfungsi agar ketika user ada kesalahan, valuenya tidak hilang --}}
                                     <input type="text" name="inventory_name" class="form-control" value="{{ old('inventory_name') }}">
                                 </div>
                             </div>
