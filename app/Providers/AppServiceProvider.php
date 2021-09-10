e525e05dee9b810e6ebbe27c8dfb53251fd08820
Muhammad Aliansyah <aliansyaahian@gmail.com>
2021-09-10 20:44:53 +0700

    Handle migration error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 // Untuk handle bug pagination laravel
 // use Illuminate\Pagination\Paginator;
 
+// Untuk handle migration error
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
     {
         // Untuk handle bug pagination laravel
         // Paginator::useBootstrap();
+
+        // Untuk handle bug Migration Error: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(191);
     }
 }
