8c975e191aeb0fa50a41bfdef6f3e34996f63c85
aliansyah <muhammad.aliansyah@divusi.com>
2021-09-15 12:13:58 +0700

    Comment script auth name & email at dashboard page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="section-body">
     Ini isi content <br>
-    Halo {{ Auth::user()->name }} <br>
-    Email Anda {{ Auth::user()->email }}
+    {{-- Halo {{ Auth::user()->name }} <br>
+    Email Anda {{ Auth::user()->email }} --}}
 </div>
 @endsection
